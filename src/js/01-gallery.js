aa69f8a54bf77506da17aac0a9824d8211e41331
M1keXl <mihayloh.com@gmail.com>
2023-03-29 13:11:24 +0300

    2

---
+++ src/js/01-gallery.js
@@ -0,0 +1,33 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+const galleryElement = document.querySelector(".gallery")
+const galleryItemsToString = galleryItems.reduce((acc, {preview, original, description}) => {
+    return acc+=`<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img loading="lazy" class="gallery__image" 
+            src="${preview}" 
+            alt="${description}" 
+        />
+   </a>
+</li>`
+}, "")
+
+galleryElement.insertAdjacentHTML("beforeend", galleryItemsToString)
+
+console.dir(SimpleLightbox)
+let gallery = new SimpleLightbox('.gallery a', {captionsData: "alt",  captionPosition:"bottom", captionDelay: 250,  scrollZoom: false});
+
+gallery.on('shown.simplelightbox', function (event) {
+  event.preventDefault();
+  console.log("Everything works fine")
+});
+
+gallery.on('error.simplelightbox', function (event) {
+	console.log(event, "Something wrong"); 
+});
+console.log(galleryItems);
+
